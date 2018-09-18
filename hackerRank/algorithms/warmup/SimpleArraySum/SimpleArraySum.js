6a1fd18905088f752196213b3288d5c6b15e5bb0
we0z <weoz@ukr.net>
2018-09-18 18:04:54 +0300

    hackerRank: SimpleArraySum(js version)

---
+++ hackerRank/algorithms/warmup/SimpleArraySum/SimpleArraySum.js
@@ -0,0 +1,13 @@
+function simpleArraySum(ar) {
+    // way 1
+    // let total = 0;
+    // ar.forEach(item => total += item);
+    // return total;
+
+    // way 2
+    return ar.reduce((total, curr) => total + curr);
+}
+
+console.log(simpleArraySum([1, 2, 3]));             //6
+console.log(simpleArraySum([1, 2, 3, 4, 10, 11]));  //31
+console.log(simpleArraySum([5, 2, 1, 4, 73, 25]));  //110
