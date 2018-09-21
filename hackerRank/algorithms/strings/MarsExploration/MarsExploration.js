06324d12fb1f2f81aa69b43b43812a4c92837547
we0z <weoz@ukr.net>
2018-09-21 17:36:00 +0300

    hackerRank: MarsExploration(js version)

---
+++ hackerRank/algorithms/strings/MarsExploration/MarsExploration.js
@@ -0,0 +1,10 @@
+function marsExploration(s) {
+    let pattern = "SOS";
+    return [...Array(s.length).keys()]
+        .filter((i) => s[i] != pattern[i % 3])
+        .length;
+}
+
+console.log(marsExploration("SOSSPSSQSSOR"));    //3
+console.log(marsExploration("SOSSOT"));          //1
+console.log(marsExploration("SOSSOSSOS"));
