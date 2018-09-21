f0fd929de359b19842f3ed2ee36e2a07734356e6
we0z <weoz@ukr.net>
2018-09-21 17:47:15 +0300

    hackerRank: MarsExploration(groovy version)

---
+++ hackerRank/algorithms/strings/MarsExploration/MarsExplorationGroovy.groovy
@@ -0,0 +1,11 @@
+package algorithms.strings.MarsExploration
+
+// https://www.hackerrank.com/challenges/mars-exploration/problem
+static def marsExploration(String s) {
+    def msg = "SOS"
+    return (0..s.length() - 1).count({ (s[it] != msg[it % 3]) })
+}
+
+println marsExploration("SOSSPSSQSSOR");    //3
+println marsExploration("SOSSOT");          //1
+println marsExploration("SOSSOSSOS");       //0
