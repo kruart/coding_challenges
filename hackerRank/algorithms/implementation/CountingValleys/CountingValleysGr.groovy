95c7f614778b5a1abef74e75b53ea9ad4f9da9d8
we0z <weoz@ukr.net>
2018-10-05 13:52:04 +0300

    hackerRank: CountingValleys(groovy version)

---
+++ hackerRank/algorithms/implementation/CountingValleys/CountingValleysGr.groovy
@@ -0,0 +1,15 @@
+package algorithms.implementation.CountingValleys
+
+static int countingValleys(int n, String s) {
+    int seaLevel = 0
+
+    return s.toList().count({
+        it == 'U'? seaLevel++ : seaLevel--
+        return it == 'U' && seaLevel == 0
+    })
+}
+
+
+println(countingValleys(8, "UDDDUDUU"))       //1
+println(countingValleys(12, "DDUUDDUDUUUD"))  //2
+println(countingValleys(2, "DD"))             //0 - because didn't leave a valley
