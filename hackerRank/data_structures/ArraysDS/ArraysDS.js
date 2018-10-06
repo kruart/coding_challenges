e71ff91df9c6dea3470c0ff177808837e528d589
we0z <weoz@ukr.net>
2018-10-06 12:19:30 +0300

    hackerRank: ArraysDS(js version)

---
+++ hackerRank/data_structures/ArraysDS/ArraysDS.js
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/challenges/arrays-ds/problem
+'use strict';
+
+function reverseArray(a) {
+    return a.reverse();
+}
+
+console.log(reverseArray([1, 4, 3, 2]));    //2, 3, 4, 1
+console.log(reverseArray([1, 4, 5, 3, 2])); //2, 3, 5, 4, 1
