9d142ee88e5448f06598d14b9ea34aa109af3da5
we0z <weoz@ukr.net>
2018-10-09 12:30:00 +0300

    hackerRank: TheHurdleRace (python version)

---
+++ hackerRank/algorithms/TheHurdleRace/the-hurdle-race.py
@@ -0,0 +1,13 @@
+def hurdleRace(k, height):
+    return max(max(height) - k, 0)
+
+
+if __name__ == '__main__':
+    nk = input().split()
+
+    n = int(nk[0])
+    k = int(nk[1])
+
+    height = list(map(int, input().rstrip().split()))
+    result = hurdleRace(k, height)
+    print(result)
