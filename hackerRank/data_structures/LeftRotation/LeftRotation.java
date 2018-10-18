7aff26705c903b1a907f142d789637f6bd0c0255
we0z <weoz@ukr.net>
2018-10-18 14:06:08 +0300

    hackerRank: LeftRotation(java version)

---
+++ hackerRank/data_structures/LeftRotation/LeftRotation.java
@@ -0,0 +1,28 @@
+package data_structures.LeftRotation;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LeftRotation {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+        int d = Integer.parseInt(nd[1]);
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[(i+n-d)%n] = aItem;
+        }
+
+        String collect = IntStream.of(a).mapToObj(String::valueOf).collect(Collectors.joining( " "));
+        System.out.println(collect);
+        scanner.close();
+    }
+}
