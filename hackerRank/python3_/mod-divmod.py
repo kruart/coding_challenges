4b05c74c2cf11e287e8d9aab79e53274e1f6e7c4
we0z <weoz@ukr.net>
2018-11-25 18:25:21 +0200

    hackerRank: Mod Divmod

---
+++ hackerRank/python3_/mod-divmod.py
@@ -0,0 +1,4 @@
+# https://www.hackerrank.com/challenges/python-mod-divmod/problem
+if __name__ == '__main__':
+    a, b = int(input()), int(input())
+    print(a // b, a % b, divmod(a, b), sep="\n")
