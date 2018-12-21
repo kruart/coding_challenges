2c5d0fcab60c0639eeeec023d9141a25f520f17c
we0z <weoz@ukr.net>
2018-12-21 18:49:30 +0200

    hackerRank: tr-command-3 (bash)

--- hackerRank/linux_shell/tr-command-2.sh
+++ hackerRank/linux_shell/tr-command-2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # delete all the lowercase characters a-z
-# https://www.hackerrank.com/challenges/text-processing-tr-1/problem
+# https://www.hackerrank.com/challenges/text-processing-tr-2/problem
 
 tr -d [a-z] # or tr -d [:lower:]
---
+++ hackerRank/linux_shell/tr-command-3.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# replace all sequences of multiple spaces with just one space.
+# https://www.hackerrank.com/challenges/text-processing-tr-3/problem
+tr -s " "
