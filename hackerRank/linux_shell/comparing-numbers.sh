cba6e24c2fb985684c12251e39cd439126218868
we0z <weoz@ukr.net>
2018-12-21 17:03:33 +0200

    hackerRank: Comparing Numbers (bash)

---
+++ hackerRank/linux_shell/comparing-numbers.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# https://www.hackerrank.com/challenges/bash-tutorials---comparing-numbers/problem
+read x
+read y
+
+if (( $x > $y )); then
+    echo "X is greater than Y"
+elif (( $x == $y)); then
+    echo "X is equal to Y"
+else
+    echo "X is less than Y"
+fi
+
+
+# second way
+#[[ $x -gt $y ]] && echo 'X is greater than Y'
+#[[ $x -eq $y ]] && echo 'X is equal to Y'
+#[[ $x -lt $y ]] && echo 'X is less than Y'
