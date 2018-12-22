0afb8d9f8f58cdb4fdc27bec063939fa5a78aa38
we0z <weoz@ukr.net>
2018-12-22 09:51:19 +0200

    hackerRank: uniq-command-4 (bash)

---
+++ hackerRank/linux_shell/uniq-command-4.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# display only those lines which are not followed or preceded by identical replications
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-uniq-command-4/problem
+uniq -u
