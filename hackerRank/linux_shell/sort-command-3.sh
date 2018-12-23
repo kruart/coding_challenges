f7735f2a210d3f0904854fadb6b7f4603263eede
we0z <weoz@ukr.net>
2018-12-23 10:05:50 +0200

    hackerRank: sort-command-3 (bash)

---
+++ hackerRank/linux_shell/sort-command-3.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Sort the lines in ascending order - so that the first line holds the numerically smallest number, and the last line holds the numerically largest number.
+# https://www.hackerrank.com/challenges/text-processing-sort-3/problem
+sort -n     # -g also works
