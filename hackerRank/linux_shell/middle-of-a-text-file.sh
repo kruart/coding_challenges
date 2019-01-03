d443b7f23fc53d70bc5d76d2eb70097f908a4794
we0z <weoz@ukr.net>
2019-01-03 17:10:48 +0200

    hackerRank: Middle of a Text File (bash)

---
+++ hackerRank/linux_shell/middle-of-a-text-file.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Display the lines (from line number 12 to 22, both inclusive) for the input file.
+# https://www.hackerrank.com/challenges/text-processing-in-linux---the-middle-of-a-text-file/problem
+head -n 22 | tail -n 11
